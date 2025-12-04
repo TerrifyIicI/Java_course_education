c38b4422428cbc899f81a3ef3cb30cd41f4b7f62
TerrifyIicI <137194109+TerrifyIicI@users.noreply.github.com>
2025-12-04 23:51:03 +0500

    Create 2.java

---
+++ Типы данных и операторов/Базовые типы данных и литералы/2.java
@@ -0,0 +1,7 @@
+public class UnicodeName {
+    public static void main(String[] args) {    
+      // Пример: имя "Anna" в Unicode
+        String name = "\u0041\u006E\u006E\u0061";
+        System.out.println(name);
+    }
+}
