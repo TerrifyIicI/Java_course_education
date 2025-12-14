dd22dd0ba3754233d3875766abf19ebe05c4768d
TerrifyIicI <137194109+TerrifyIicI@users.noreply.github.com>
2025-12-14 17:14:21 +0500

    Create 1.java

---
+++ Потоки ввода\вывода/Класс Scanner/1.java
@@ -0,0 +1,29 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Arina
+ */
+public class Main {
+    public static void main(String[] args) {
+        // Укажите путь к файлу, который вы хотите прочитать
+        File file = new File("example.txt");
+
+        try (Scanner scanner = new Scanner(file)) {
+            System.out.println("Содержимое файла:");
+
+            // Чтение файла построчно
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                System.out.println(line);
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("Файл не найден: " + e.getMessage());
+        }
+    }
+}
+```
+
+Код готов к сдаче!
